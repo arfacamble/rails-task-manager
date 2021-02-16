135f7f52e6a75f181a5c590e7c6c015a6ba503d3
Arthur Campbell <arfa.camble@gmail.com>
2021-02-16 15:10:10 +0000

    all sorts

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -17,9 +17,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit; end
+
+  def update
+    @task.update task_params
+    redirect_to task_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
